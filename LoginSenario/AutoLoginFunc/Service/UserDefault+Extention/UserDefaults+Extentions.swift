99abc4b80a14053235ec3372af8240e7f7d9b7a7
Admin <admin@Admins-Mac-mini.lan>
2019-12-22 12:24:14 +0600

    a log in system added

---
+++ LoginSenario/AutoLoginFunc/Service/UserDefault+Extention/UserDefaults+Extentions.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+extension UserDefaults{
+    
+ func setValueForLogin(value:Bool?){
+        
+        if value != nil{
+            
+            UserDefaults.standard.set(value, forKey: "email")
+        }
+        else{
+            
+            UserDefaults.standard.removeObject(forKey: "email")
+        }
+        
+        UserDefaults.standard.synchronize()
+    }
+    
+    
+    func getValueofLogin()->Bool?{
+        return UserDefaults.standard.value(forKey: "email") as? Bool
+    }
+}
+
